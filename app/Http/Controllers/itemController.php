70cf4a7dc57982e302a71c348060a906e30c34ac
artemama <artsiom.ivahno@gmail.com>
2019-01-11 17:59:03 +0300

    first

---
+++ app/Http/Controllers/itemController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class itemController extends Controller
+{
+	public function index()
+	{
+		echo index;
+	       return view('post.index');
+	}
+}
---
+++ app/Http/Controllers/manufacturerController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class manufacturerController extends Controller
+{
+	public function index()
+	{
+		echo index;
+	       return view('post.index');
+	}
+}
